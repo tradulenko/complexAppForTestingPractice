9d784cc79b4166a54a271b66a06df2f3c83b3337
shushkovaira <shushkovaira@gmail.com>
2023-01-02 20:01:43 +0200

    Should be added to TAF:
    lesson 1, version 5
    with fixed issues

--- src/main/java/libs/ConfigProperties.java
+++ src/main/java/libs/ConfigProperties.java
@@ -9,13 +9,4 @@ public interface ConfigProperties extends Config {
         long TIME_FOR_EXPLICIT_WAIT_LOW();
         long TIME_FOR_EXPLICIT_WAIT_HIGHT();
 
-        String DATA_FILE();
-        String DATA_FILE_PATH();
-        String Oracle();
-        String MySQL();
-        String sqlServer();
-
-        String MySQL_DB();
-        String MySQL_DB_USER();
-        String MySQL_DB_PASSWORD();
 }
